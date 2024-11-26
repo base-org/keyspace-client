84323a20b66018932d1294c31e9c0fbcc8d22317
Niran Babalola <niran@niran.org>
2024-11-25 21:45:06 -0600

    Remove more traces of keyspace-viem

--- scripts/lib/client.ts
+++ scripts/lib/client.ts
@@ -1,40 +1,25 @@
 import { bundlerActions, BundlerClient } from "permissionless";
-import { Chain, Client, createPublicClient, http, HttpTransportConfig, PublicActions, PublicClient, PublicRpcSchema, Transport } from "viem";
-import { baseSepolia, Prettify, sepolia } from "viem/chains";
+import { createPublicClient, http, PublicClient } from "viem";
+import { baseSepolia, sepolia } from "viem/chains";
 import { publicActionsL2 } from 'viem/op-stack';
-import { DebugActions, debugActions } from "../../src/keyspace-viem/decorators/debug";
-import { DebugRpcSchema } from "../../src/keyspace-viem/actions/types";
 
 export const chain = baseSepolia;
 
-export type DebugClient<
-  transport extends Transport = Transport,
-  chain extends Chain | undefined = Chain | undefined,
-> = Prettify<
-  Client<
-    transport,
-    chain,
-    undefined,
-    PublicRpcSchema & DebugRpcSchema,
-    PublicActions<transport, chain> & DebugActions
-  >
->
-
-export const client: DebugClient = createPublicClient({
+export const client: PublicClient = createPublicClient({
   chain,
   transport: http(
     process.env.RPC_URL || ""
   ),
-}).extend(debugActions());
+});
 
-export const masterClient: DebugClient = client;
+export const masterClient: PublicClient = client;
 
-export const l1Client: DebugClient = createPublicClient({
+export const l1Client: PublicClient = createPublicClient({
   chain: sepolia,
   transport: http(
     process.env.L1_RPC_URL || ""
   ),
-}).extend(debugActions()).extend(publicActionsL2());
+}).extend(publicActionsL2());
 
 export const bundlerClient: BundlerClient = createPublicClient({
   chain,
